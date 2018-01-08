2d9623c9087cea27bef910b0fddb3aa43b53a599
De-etz <adityakumarsingh2100@gmail.com>
2018-01-08 18:30:41 -0500

    Auto-update

--- bot.js
+++ bot.js
@@ -4,25 +4,20 @@ var Bot = require('node-telegram-bot-api');
 var bot;
 
 if(process.env.NODE_ENV === 'production') {
-	bot = new Bot(token);
-	bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
 }
 else {
-	bot = new Bot(token, { polling: true });
+  bot = new Bot(token, { polling: true });
 }
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-const fam = "/fam";
-
-
 bot.onText(/^/, function (msg) {
-	if (msg.text.toString().toLowerCase().includes("namaste") || msg.text.toString().toLowerCase().includes("hi")) {
-		var name = msg.from.first_name;
-		bot.sendMessage(msg.chat.id, 'Namaste, ' + name + ' Ji!').then(function () {
-		// reply sent!
-		}
-	});
+  var name = msg.from.first_name;
+  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
+    // reply sent!
+  });
 });
 
 module.exports = bot;
