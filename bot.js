b039c76bf67f9677b7f0b115ae497490615a72d6
De-etz <adityakumarsingh2100@gmail.com>
2018-01-08 18:58:07 -0500

    Auto-update

--- bot.js
+++ bot.js
@@ -3,12 +3,19 @@ var token = process.env.TOKEN;
 var Bot = require('node-telegram-bot-api');
 var bot;
 
+var intro = 0;
+
 if(process.env.NODE_ENV === 'production') {
 	bot = new Bot(token);
 	bot.setWebHook(process.env.HEROKU_URL + bot.token);
 }
 else {
 	bot = new Bot(token, { polling: true });
+}
+
+if (intro == 0) {
+	bot.sendMessage(msg.chat.id, "Baba Ramdev updated succesfully.");
+	intro = 1;
 }
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
@@ -23,5 +30,6 @@ bot.on('message', (msg) => {
 	} 
     
 });
+
 
 module.exports = bot;
