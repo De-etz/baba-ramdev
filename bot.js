2bdd81bb22af94c3c5d47f783d51d952b2692e77
De-etz <adityakumarsingh2100@gmail.com>
2018-01-08 20:02:39 -0500

    Auto-update

--- bot.js
+++ bot.js
@@ -43,9 +43,14 @@ bot.on('message', (msg) => {
 
 bot.onText(/\/displayInfo, (msg) => {
 	bot.sendMessage(msg.chat.id, JSON.stringify(msg));
+}
 
 bot.onText(/\/slap/, (msg) => {
-	bot.sendMessage(msg.chat.id, msg.from.first_name);
+	try {
+		bot.sendMessage(msg.chat.id, generateSlap(msg.from.first_name, msg.reply_to_message.from.first_name));
+    } catch (err) {
+		bot.sendMessage(msg.chat.id, 'Error: ' + err.message);
+	}
 });
 
 
